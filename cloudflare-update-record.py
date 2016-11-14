d8c6173581e3ae15e20a730ddbca0d9ea7205741
Ben Sparks <bsparks@kirklands.com>
2016-11-14 14:39:09 -0600

    clean up

--- cloudflare-update-record.py
+++ cloudflare-update-record.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 import smtplib
 import json
-import sqlite3
 import logging
 import socket
 import dns.resolver
 import os
-import sys
 import CloudFlare
 import CloudFlare.exceptions
 from datetime import datetime
 from email.mime.text import MIMEText
-from contextlib import closing
 
 def GetRequired():
     with open(os.path.join(dir_path, 'required.json'), 'r') as fin:
@@ -28,20 +25,6 @@ def GetCurrentIP():
     resolver.nameservers=[socket.gethostbyname('resolver1.opendns.com')]
     return resolver.query('myip.opendns.com')[0]
 
-#def GetLastKnownIP():
-    #try:
-        #con = sqlite3.connect(os.path.join(dir_path, 'cloudflareDNS.sqlite'))
-        #with closing(con.cursor()) as cur:
-            #cur.execute('SELECT ip FROM currentIP')
-            #return cur.fetchone()[0]
-    #except sqlite3.Error as e:
-        #if con:
-            #con.rollback()
-        #log('Error {0}:'.format(e.args[0]))
-    #finally:
-        #if con:
-            #con.close()
-
 def GetRecordIDs(cf, zone_id):
     try:
         dns_records = []
@@ -49,8 +32,8 @@ def GetRecordIDs(cf, zone_id):
             params = {'name':record, 'match':'all', 'type':'A'}
             dns_records.append(cf.zones.dns_records.get(zone_id, params=params))
     except CloudFlare.exceptions.CloudFlareAPIError as e:
-        log('/zones/dns_records %s - %d %s - api call failed' % (dns_name, e, e))
-        exit
+        log("/zones/dns_records {0} - {1} {2} - api call failed".format(dns_name, e, e))
+        exit(0)
 
     return dns_records
 
@@ -59,67 +42,70 @@ def GetZoneID(cf):
         params = {'name':required["zone_name"]}
         zones = cf.zones.get(params=params)
     except CloudFlare.exceptions.CloudFlareAPIError as e:
-        log('/zones %d %s - api call failed' % (e, e))
+        log("/zones {0} {1} - api call failed".format(e, e))
         exit
     except Exception as e:
-        log('/zones.get - %s - api call failed' % (e))
-        exit
+        log("/zones.get - {0} - api call failed".format(e))
+        exit(0)
 
     if len(zones) == 0:
-        log('/zones.get - %s - zone not found' % (zone_name))
-        exit
+        log("/zones.get - {0} - zone not found".format(zone_name))
+        exit(0)
     if len(zones) != 1:
-        log('/zones.get - %s - api call returned %d items' % (zone_name, len(zones)))
-        exit
+        log("/zones.get - {0} - api call returned %d items".format(zone_name, len(zones)))
+        exit(0)
 
     return zones[0]["id"]
 
+def SendUpdateEmail(msg):
+    gmailAddress = required['sender']
+    gmailPassword = required['password'] #App Specific Password
+
+    fromSender = gmailAddress
+    toRecipients = required['recipients']
+
+    msg_subject = 'DNS UPDATE:'
+    msg_text = msg
+
+    msg = MIMEText(msg_text)
+    msg['Subject'] = msg_subject
+    msg['From'] = fromSender
+    msg['To'] = ", ".join(toRecipients)
+    s = smtplib.SMTP_SSL('smtp.gmail.com', '465')
+    s.login(gmailAddress, gmailPassword)
+    s.sendmail(fromSender, toRecipients, msg.as_string())
+    s.quit()
+
 def UpdateRecord(cf, ids, currentIP):
     for dns_record in ids:
-            knownIP = dns_record[0]['content']
-            dns_name = dns_record[0]["name"]
-            dns_record_id = dns_record[0]["id"]
-            ip_address_type = dns_record[0]["type"]
-            zone_id = dns_record[0]["zone_id"]
-            #print currentIP
-            #print knownIP
-            #print("{0} {1} {2} {3}".format(dns_name, dns_record_id, ip_address, ip_address_type))
-
-            #print(dns_record[0])
-            if currentIP == knownIP:
-                log("IP, {0}, has not changed for {1}.".format(knownIP, dns_name))
-                exit
-            else:
-                dns_record[0]['content'] = currentIP.address
-                try:
-                    #dns_record = cf.zones.dns_records.put(zone_id, dns_record_id, data=dns_record[0])
-                    msg = "UPDATED: {0} {1} -> {2}".format(dns_name, knownIP, currentIP)
-                    log(msg)
-                    SendUpdateEmail(msg)
-                except CloudFlare.exceptions.CloudFlareAPIError as e:
-                    log("/zones.dns_records.put {0} - {1} {2} - api call failed".format(dns_name, e, e))
-                    exit                
+        knownIP = dns_record[0]['content']
+        dns_name = dns_record[0]["name"]
+        dns_record_id = dns_record[0]["id"]
+        ip_address_type = dns_record[0]["type"]
+        zone_id = dns_record[0]["zone_id"]
+        #print("{0} {1} {2} {3}".format(dns_name, dns_record_id, ip_address, ip_address_type))
+        if currentIP == knownIP:
+            log("IP, {0}, has not changed for {1}.".format(knownIP, dns_name))
+            exit
+        else:
+            dns_record[0]['content'] = currentIP.address
+            try:
+                #dns_record = cf.zones.dns_records.put(zone_id, dns_record_id, data=dns_record[0])
+                msg = "UPDATED: {0} {1} -> {2}".format(dns_name, knownIP, currentIP)
+                log(msg)
+                SendUpdateEmail(msg)
+            except CloudFlare.exceptions.CloudFlareAPIError as e:
+                log("/zones.dns_records.put {0} - {1} {2} - api call failed".format(dns_name, e, e))
+                exit                
 
 if __name__ == "__main__":
     dir_path = os.path.dirname(os.path.realpath(__file__))
     logging.basicConfig(filename=os.path.join(dir_path, 'cloudflare.log'),level=logging.DEBUG)
-    log('Check Initiated')
-    #print(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-    #print(sys.path.insert(0, os.path.abspath('..')))
+    log('DNS Check Initiated')
     required = GetRequired()
     currentIP = GetCurrentIP()
-    #print(required["auth_email"])
-    #print(required["auth_key"])
     cf = CloudFlare.CloudFlare(email=required["auth_email"], token=required["auth_key"])
     zone_id = GetZoneID(cf)
-    #print(required["zone_name"])
-    #print(zone_id)
     ids = GetRecordIDs(cf, zone_id)
     UpdateRecord(cf, ids, currentIP)
-
-    
-    #SetOutageInfo(now)
-    #SendGmailMsg(required, now)
-
-
-
+    log('DNS Check Complete')
